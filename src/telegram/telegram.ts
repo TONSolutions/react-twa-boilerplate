9fa8e9d2ac50406530c0bd14f1b57e463fa4e0e9
Alex Zhigulev <strdxxx@gmail.com>
2024-03-14 17:46:24 +0400

    fix(telegram.ts): Fix user data resolving

--- src/telegram/telegram.ts
+++ src/telegram/telegram.ts
@@ -1,4 +1,3 @@
-import { URLSearchParams } from "url";
 import { Telegram } from "./web-app";
 import { TelegramWebAppData } from "../models";
 
@@ -20,31 +19,43 @@ export const loadTelegramWebAppData = (): TelegramWebAppData => {
 }
 
 export const loadFromTelegramLib = (): TelegramWebAppData => {
-    if (webAppHandler === null) {
-        throw new Error('Telegram WebApp is not loaded.');
-    }
+  if (webAppHandler === null) {
+    throw new Error('Telegram WebApp is not loaded.');
+  }
 
-    const initData = webAppHandler.initDataUnsafe;
-    return { ...initData, platform: webAppHandler.platform };
+  const initData = webAppHandler.initDataUnsafe;
+  return { ...initData, platform: webAppHandler.platform };
 }
 
 export const loadFromHash = (): TelegramWebAppData => {
-    const documentHash = document.location.hash;
+  const documentHash = document.location.hash;
 
-    if (documentHash === '') {
-        throw new Error('Telegram WebApp is not loaded.');
-    }
+  if (documentHash === '') {
+    throw new Error('Telegram WebApp is not loaded.');
+  }
 
-    const hash = decodeURIComponent(decodeURIComponent(document.location.hash.substring(1)));
-    const urlParams = new URLSearchParams(hash);
+  const hash = decodeURIComponent(
+    decodeURIComponent(document.location.hash.substring(1)),
+  );
+  const urlParams = new URLSearchParams(hash);
+  const tgWebAppData = urlParams.get('tgWebAppData');
 
-    return {
-        user: urlParams.get('user') ? JSON.parse(urlParams.get('user') as string) : undefined,
-        chat_type: urlParams.get('chat_type') as Telegram.InitData['chat_type'],
-        chat_instance: urlParams.get('chat_instance') || undefined,
-        platform: urlParams.get('tgWebAppPlatform') || 'unknown',
-        auth_date: urlParams.get('auth_date') ? parseInt(urlParams.get('auth_date') as string) : 0,
-        hash: urlParams.get('hash') || '',
-        start_param: urlParams.get('start_param') || undefined,
-    };
+  if (tgWebAppData === null) {
+    throw new Error('Telegram WebApp is not loaded.');
+  }
+
+  const userParams = new URLSearchParams(tgWebAppData);
+  return {
+    user: userParams.get('user')
+      ? JSON.parse(userParams.get('user') as string)
+      : undefined,
+    chat_type: urlParams.get('chat_type') as Telegram.InitData['chat_type'],
+    chat_instance: urlParams.get('chat_instance') || undefined,
+    platform: urlParams.get('tgWebAppPlatform') || 'unknown',
+    auth_date: urlParams.get('auth_date')
+      ? parseInt(urlParams.get('auth_date') as string)
+      : 0,
+    hash: urlParams.get('hash') || '',
+    start_param: urlParams.get('start_param') || undefined,
+  };
 }
